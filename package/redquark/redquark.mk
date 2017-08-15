7bc38dc44d8bba182229670fbfa229477836c47d
Chris Smith <chris@zxdesign.info>
2017-08-15 13:32:39 +0100

    Fixed redquark config.

---
+++ package/redquark/redquark.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# the64
+#
+################################################################################
+
+REDQUARK_SITE_METHOD = git
+REDQUARK_GIT_SUBMODULES = YES
+REDQUARK_SITE = ssh://git@bitbucket.org/ZXDesign/the64-firmware
+REDQUARK_VERSION = 0.1.2-pre
+REDQUARK_LICENSE = the64 license
+REDQUARK_LICENSE_FILES = README
+REDQUARK_INSTALL_STAGING = NO
+
+$(eval $(autotools-package))
