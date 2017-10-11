8db58482af3018d86e6ba68556600c13ca287baf
Chris Smith <chris@zxdesign.info>
2017-10-11 18:02:07 +0100

    Create archive of nand boot partition contents.

--- board/redquark/redquarkone-prod/post-build.sh
+++ board/redquark/redquarkone-prod/post-build.sh
@@ -3,10 +3,53 @@
 # $2    path of boot.cmd
 # $3    output directory for boot.scr
 
-MKIMAGE=$HOST_DIR/usr/bin/mkimage
-
-$MKIMAGE -A arm -O linux -T script -C none -d $2 $3/boot.scr
+#MKIMAGE=$HOST_DIR/usr/bin/mkimage
+#$MKIMAGE -A arm -O linux -T script -C none -d $2 $3/boot.scr
+echo "A1 $1"
+echo "A2 $2"
+echo "A2 $3"
 
 if [ -e $BINARIES_DIR/script.bin ]; then
 	cp $BINARIES_DIR/script.bin $3/script.bin
 fi
+
+BOARD_DIR="$(dirname $0)"
+
+# Copy any required boot file
+cp ${BOARD_DIR}/boot/* ${TARGET_DIR}/boot/
+
+# Create NAND boot partition filesystem
+BOOT_FS_ARCHIVE=${BINARIES_DIR}/boot.tgz
+BOOT_FS_TMP=${BUILD_DIR}/_boot
+if [ ! -d ${BOOT_FS_TMP} ]; then
+    mkdir ${BOOT_FS_TMP}
+fi
+cp -r    ${BOARD_DIR}/nanda/*       ${BOOT_FS_TMP}
+cp       ${BINARIES_DIR}/u-boot.bin ${BOOT_FS_TMP}/linux/
+rm -f    ${BOOT_FS_ARCHIVE}
+tar cvzf ${BOOT_FS_ARCHIVE} -C ${BOOT_FS_TMP} .
+
+## Create NAND boot partition filesystem
+#BOOT_FS_IMAGE=${BINARIES_DIR}/boot.msdos
+#BOOT_FS_TMP=${BUILD_DIR}/_boot
+#mkdir -d ${BOOT_FS_TMP}
+#cp -r ${BOARD_DIR}/nanda/ ${BOOT_FS_TMP}
+#cp    ${BINARIES_DIR}/u-boot.bin ${BOOT_FS_TMP}/linux/
+#PATH="${HOST_DIR}/bin:${HOST_DIR}/sbin:${HOST_DIR}/usr/bin:${HOST_DIR}/usr/sbin:$PATH"
+##rm -f ${BOOT_FS_IMAGE}
+#mke2img -d ${BOOT_FS_TMP}/ -o ${BOOT_FS_IMAGE}
+## Shrink the boot FS to within 1MB
+#e2fsck boot.msdos
+#resize2fs boot.msdos 900K
+
+
+## Create user profile partition FS
+#USER_FS_IMAGE=${BINARIES_DIR}/user.ext2
+#USER_FS_TMP=${BUILD_DIR}/_user_profile
+#mkdir -d ${USER_FS_TMP}
+#PATH="${HOST_DIR}/bin:${HOST_DIR}/sbin:${HOST_DIR}/usr/bin:${HOST_DIR}/usr/sbin:$PATH"
+#rm -f ${BOOT_FS_IMAGE}
+#mke2img -d ${BOOT_FS_TMP}/ -o ${BOOT_FS_IMAGE}
+## Shrink the boot FS to within 1MB
+#e2fsck boot.msdos
+#resize2fs boot.msdos 900K
